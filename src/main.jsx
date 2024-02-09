a1792309d95097c83dfe42981826ab7298cd54c6
nhetsko <nadiahecko@gmail.com>
2024-02-09 14:16:23 +0200

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "modern-normalize";
-import App from "./components/App/App";
+import App from "./components/App/App.jsx";
 import "./index.css";
+// import "../src/App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
