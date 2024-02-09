d206ee41c81dc9a36bb141216f1e757d158f3b19
nhetsko <nadiahecko@gmail.com>
2024-02-09 13:50:22 +0200

    add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
-import  Profile  from '../Profile/Profile';
-import userData from '../../userData.json'; 
+import Profile from '../Profile/Profile';
+import userData from '../../userData.json';
 import FriendList from '../FriendList/FriendList';
 import friends from '../../friends.json';
 import TransactionHistory from '../Transaction/TransactionHistory';
